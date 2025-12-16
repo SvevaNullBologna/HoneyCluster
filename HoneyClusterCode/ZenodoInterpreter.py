99f93abab820b09d16be239560013b129dff834b
SvevaNullBologna <silviaangela.carollo@studio.unibo.it>
2025-12-16 14:05:42 +0100

    fixed some stuff

--- HoneyClusterCode/ZenodoInterpreter.py
+++ HoneyClusterCode/ZenodoInterpreter.py
@@ -1,15 +1,13 @@
 import json
-import os
 import logging
-import gzip
 import file_worker
+import os
 
 class ZenodoInterpreter:
-    def __init__(self, zenodo_local_path: str , extracted_zenodo_local_path : str | None , extracted_files_tab: str | None ):
+    def __init__(self, zenodo_local_path: str , extracted_zenodo_local_path : str | None):
         logging.info("Zenodo Interpreter started.")
         if file_worker.contains_file_type(zenodo_local_path, ".gz") > 0 :
-            self.zenodo_path, self.extracted_zenodo_path, self.extracted_files_tab = file_worker.extract_gz_files(zenodo_local_path, extracted_zenodo_local_path, extracted_files_tab)
-
+            self.zenodo_path, self.extracted_zenodo_path = file_worker.extract_gz_files(zenodo_local_path, extracted_zenodo_local_path)
             self.zenodo_keys = {"session_id", "dst_ip_identifier","dst_host_identifier","src_ip_identifier","eventid","timestamp","message","protocol","geolocation_data","src_port","sensor", "arch", "duration", "ssh_client_version", "username", "password", "macCS", "encCS", "keyAlgs", "keyAlgs"}
             self.zenodo_geolocation_keys = {"postal_code", "continent_code", "country_code3", "region_name", "latitude", "longitude", "country_name", "timezone", "country_code2", "region_code", "city_name"}
 
@@ -20,7 +18,7 @@ class ZenodoInterpreter:
         try:
             with open(filename, "r", encoding="utf-8") as json_file:
                 data = json.load(json_file)
-                print(data)
+                
             return data
         except Exception as e:
             logging.error(f"Failed to parse JSON {filename}: {e}")
@@ -32,8 +30,8 @@ class ZenodoInterpreter:
 
 def main() -> None:
     logging.basicConfig(level=logging.DEBUG)
-    z_interpreter = ZenodoInterpreter("C:\\Users\\Sveva\\Documents\\GitHub\\zenodo_dataset", "C:\\Users\\Sveva\\Desktop\\estratti", None)
-
+    z_interpreter = ZenodoInterpreter("C:\\Users\\Sveva\\Documents\\GitHub\\zenodo_dataset", "C:\\Users\\Sveva\\Desktop\\estratti")
+    z_interpreter.interpret_single_zenodo_json(os.path.join(z_interpreter.extracted_zenodo_path, "cyberlab_2019-05-18.json"))
 
 if __name__ == "__main__":
     main()
--- HoneyClusterCode/file_worker.py
+++ HoneyClusterCode/file_worker.py
@@ -5,26 +5,10 @@ import shutil
 
 from pathlib import Path
 
-
-from typing import Callable, TextIO
-
-
-
-def add_row_to_file(filename: str, row: str):
-    with open(filename, "a", encoding="utf-8") as f:
-        f.write(row + "\n")
-
-
-def read_file(filename: str,function_on_file: Callable[[TextIO], None]):
-    with open(filename, "r", encoding="utf-8") as f:
-        function_on_file(f)
-
-
 def contains_file_type(folder: str, extension: str)-> int:
     folder_path = Path(folder)
     files = list(folder_path.glob(f"*{extension}"))
     return len(files)
-
 
 
 def check_directory(path: str | None, creation: bool) -> bool:
@@ -43,66 +27,37 @@ def check_directory(path: str | None, creation: bool) -> bool:
         return True
 
 
-
-def extract_gz_files(origin_path: str, destination_path: str | None, extracted_gz_list_file: str | None = None) -> (str, str, str):
+def extract_gz_files(origin_path: str, destination_path: str | None) -> (str, str):
     """
     Estrae tutti i file .gz nella cartella specificata.
     """
     if not destination_path :
         destination_path = origin_path
 
-    if not extracted_gz_list_file:
-        extracted_gz_list_file = "extracted_gz.txt"
-
-    if not os.path.exists(extracted_gz_list_file):
-        open(extracted_gz_list_file, "a").close()
-
     if not (check_directory(origin_path, False) and check_directory(destination_path, True)):
-        return
+        return None,None
 
     logging.info(f"Extracting {origin_path} to {destination_path}")
-    extracted_files = __get_extracted_files_list(extracted_gz_list_file)
 
     for filename in os.listdir(origin_path):
-        if not filename.endswith(".gz") and not __has_already_been_extracted(extracted_files, filename):
+        if not filename.endswith(".gz") or __has_already_been_extracted(destination_path, Path(filename).with_suffix("").name):
+            logging.info(f"skipping {filename}")
             continue
 
         src_path = os.path.join(origin_path, filename)
         dst_path = os.path.join(destination_path, Path(filename).with_suffix("").name)
-
-        logging.info(f"Decompressione: {src_path} -> {dst_path}")
-
         try:
             with gzip.open(src_path, "rb") as f_in, open(dst_path, "wb") as f_out:
                 shutil.copyfileobj(f_in, f_out)
-
-            __update_extracted(extracted_gz_list_file, filename, 1)
+            logging.info(f"Decompressione di {filename} in {src_path} -> {dst_path}")
         except (OSError, EOFError) as e:
             logging.error(f"Errore decompressione {filename}: {e}")
-            __update_extracted(extracted_gz_list_file, filename, 0)
     logging.info(f"End of extraction of gz files")
-    return origin_path, destination_path, extracted_gz_list_file
+    return origin_path, destination_path
 
-def __get_extracted_files_list(extracted_file: str):
-    list_of_files = []
-    if not os.path.exists(extracted_file):
-        return list_of_files
+def __has_already_been_extracted(destination_path, filename: str) -> bool:
+    return os.path.exists(os.path.join(destination_path, filename))
 
-    with open(extracted_file, "r", encoding="utf-8") as f:
-        for line in f:
-            name, *_ = line.strip().split(" ", 1)
-            list_of_files.append(name)
-    return list_of_files
-
-def __has_already_been_extracted(list_of_files: list[str], filename: str) -> bool:
-    return filename in list_of_files
-
-def __update_extracted(extracted_file: str, filename: str, result_of_extraction_code: int):
-    match result_of_extraction_code:
-        case 1:
-            __append_to_file(extracted_file, f"{filename} EXTRACTED")
-        case 0:
-            __append_to_file(extracted_file, f"{filename} ERROR")
 
 
 def __append_to_file(filename: str, row: str) -> None:
@@ -113,7 +68,7 @@ def main():
     logging.basicConfig(level=logging.INFO)
 
     test_origin = "C:\\Users\\Sveva\\Documents\\GitHub\\zenodo_dataset"
-    test_dest = "C:\\Users\\Sveva\\Desktop\\estratti"
+    test_dest = "C:\\Users\\Sveva\\Desktop\\new"
 
     extract_gz_files(test_origin, test_dest)
 
