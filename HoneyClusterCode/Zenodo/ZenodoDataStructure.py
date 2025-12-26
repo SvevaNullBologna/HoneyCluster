269aab8be65bf61a13a71738380304110b9c9acf
SvevaNullBologna <silviaangela.carollo@studio.unibo.it>
2025-12-26 13:39:08 +0100

    fixed data structure

--- HoneyClusterCode/Zenodo/ZenodoDataStructure.py
+++ HoneyClusterCode/Zenodo/ZenodoDataStructure.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from typing import Optional
-from datetime import datetime
+from typing import Optional,List
+
 """
                     Field                             Description
     ===============================   ===========================================================
@@ -44,6 +44,99 @@ from datetime import datetime
                                 "longitude", "country_name", "timezone", "country_code2", "region_code","city_name"}
 
 """
+"""
+Legenda:
+[ ... ] = lista
+{ ... } = dizionario
+
+ogni LogFile contiene qualcosa del tipo:
+     [ 
+        {
+            "identificatore che è uguale al valore del session id": [
+                {
+                    "session_id":"f83815b1d845",
+                    "dst_host_identifier":"31790014bd4d8ec9a6273dcd7335f4569c83985f98bf1ba97e029833d58e6c81",
+                    "eventid":"cowrie.session.connect",
+                    "timestamp":"2019-06-26T00:07:20.921797Z",
+                    "src_ip_identifier":"a2bd07cfe8104e4bd7b82ad7eb1868029d93a151a96bbcae2d4b746bde838bde",
+                    "dst_ip_identifier":null,
+                    "message":"New connection: a2bd07cfe8104e4bd7b82ad7eb1868029d93a151a96bbcae2d4b746bde838bde:37412 (10.244.2.40:2222) [session: f83815b1d845]",
+                    "protocol":"ssh",
+                    "src_port":37412,
+                    "sensor":"cowrie-deployment-2nwj2",
+                    "geolocation_data": { * guarda sotto * },
+                    "arch":null,
+                    "duration":null,
+                    "ssh_client_version":null,
+                    "username":null,
+                    "password":null,
+                    "hasshAlgorithms":null,
+                    "macCS":null,
+                    "langCS":null,
+                    "compCS":null,
+                    "encCS":null,
+                    "hassh":null,
+                    "kexAlgs":null,
+                    "keyAlgs":null,
+                    "fingerprint":null,
+                    "key":null,
+                    "type":null,
+                    "outfile":null,
+                    "destfile":null,
+                    "duplicate":null,
+                    "shasum":null,
+                    "url":null,
+                    "ttylog":null,
+                    "size":null,
+                    "filename":null,
+                    "data":null
+                },
+                {
+                    ...
+                }
+            ]
+        },
+        { 
+            ... 
+        },
+        { 
+            ...
+        }
+    ]
+     
+    * * 
+  
+  "geolocation_data": {
+    "country_name": "Netherlands",
+    "continent_code": "EU",
+    "timezone": "Europe/Amsterdam",
+    "region_code": "ZH",
+    "country_code2": "NL",
+    "location": {
+      "lon": 4.663,
+      "lat": 51.8656
+    },
+    "region_name": "South Holland",
+    "latitude": 51.8656,
+    "country_code3": "NL",
+    "ip": "00a59ce4892d5315f8eaec9baaf8c31ba1496edf2b2b954d13b8f896301df9fe",
+    "postal_code": "2951",
+    "city_name": "Alblasserdam",
+    "longitude": 4.663
+  }
+  
+  
+  
+  Cosa vogliamo ottenere noi?
+  
+  "valore data del dataLog" : {
+        "valore session_id" : 
+        [ "event_id": .... ,
+           tutti i valori non nulli
+        ]
+  }
+ 
+"""
 
 @dataclass
 class GeolocationData:
@@ -59,73 +152,163 @@ class GeolocationData:
     region_code: Optional[str] = None
     city_name: Optional[str] = None
 
-    def print_geolocation(self):
-        print("Geolocation:")
-        if self.country_name:
-            print(f"  Country  : {self.country_name}")
+    def is_null(self):
+        return not any([self.postal_code,self.continent_code,self.country_code3,self.region_name,self.latitude,self.longitude,self.country_name,self.timezone,self.country_code2,self.region_code,self.city_name])
+
+    def __str__(self):
+        parts = ["GEOLOCATION:"]
+        if self.postal_code:
+            parts.append(f'postal code: {self.postal_code}')
         if self.continent_code:
-            print(f"  Continent: {self.continent_code}")
+            parts.append(f'continent code: {self.continent_code}')
+        if self.country_code3:
+            parts.append(f'country code3: {self.country_code3}')
+        if self.region_name:
+            parts.append(f'region name: {self.region_name}')
+        if self.latitude:
+            parts.append(f'latitude: {self.latitude}')
+        if self.longitude:
+            parts.append(f'longitude: {self.longitude}')
+        if self.country_name:
+            parts.append(f'country name: {self.country_name}')
+        if self.timezone:
+            parts.append(f'timezone: {self.timezone}')
+        if self.country_code2:
+            parts.append(f'country code2: {self.country_code2}')
+        if self.region_code:
+            parts.append(f'region code: {self.region_code}')
         if self.city_name:
-            print(f"  City     : {self.city_name}")
-        if self.latitude and self.longitude:
-            print(f"  Lat/Lon  : {self.latitude}, {self.longitude}")
+            parts.append(f'city name: {self.city_name}')
+        return "\n".join(parts)
 
 
 @dataclass
 class ZenodoEvent:
     # === REQUIRED ===
-    session_id: str
     eventid: str
 
     # === OPTIONAL ===
+    dst_host_identifier: Optional[str] = None
     timestamp: Optional[str] = None
     src_ip_identifier: Optional[str] = None
     dst_ip_identifier: Optional[str] = None
-    dst_host_identifier: Optional[str] = None
     message: Optional[str] = None
     protocol: Optional[str] = None
-    src_port: Optional[int] = None
+    scr_port: Optional[str] = None
     sensor: Optional[str] = None
+    geolocation_data: Optional[GeolocationData] = None
     arch: Optional[str] = None
-    duration: Optional[float] = None
+    duration: Optional[str] = None
     ssh_client_version: Optional[str] = None
     username: Optional[str] = None
     password: Optional[str] = None
+    hasshAlgorithms: Optional[str] = None
     macCS: Optional[str] = None
+    langCS: Optional[str] = None
+    compCS: Optional[str] = None
     encCS: Optional[str] = None
+    hassh: Optional[str] = None
     kexAlgs: Optional[str] = None
     keyAlgs: Optional[str] = None
-    geolocation_data: Optional[GeolocationData] = None
+    fingerprint: Optional[str] = None
+    key: Optional[str] = None
+    type: Optional[str] = None
+    outfile: Optional[str] = None
+    destfile: Optional[str] = None
+    duplicate: Optional[str] = None
+    shasum: Optional[str] = None
+    url: Optional[str] = None
+    ttylog: Optional[str] = None
+    size: Optional[str] = None
+    filename: Optional[str] = None
+    data: Optional[str] = None
 
-
-    def print_event(self):
-        print("-" * 50)
-        print(f"Event ID   : {self.eventid}")
-        print(f"Session ID : {self.session_id}")
-
+    def __str__(self):
+        parts = ["ZENODO EVENT:", f'eventid: {self.eventid}']
+        if self.dst_host_identifier :
+            parts.append(f'dst_host_identifier: {self.dst_host_identifier}')
         if self.timestamp:
-            print(f"Timestamp  : {self.timestamp}")
-        if self.protocol:
-            print(f"Protocol   : {self.protocol}")
-        if self.src_port is not None:
-            print(f"Src Port   : {self.src_port}")
+            parts.append(f'timestamp: {self.timestamp}')
+        if self.src_ip_identifier:
+            parts.append(f'src_ip_identifier: {self.src_ip_identifier}')
+        if self.dst_ip_identifier:
+            parts.append(f'dst_ip_identifier: {self.dst_ip_identifier}')
         if self.message:
-            print(f"Message    : {self.message}")
-
-        if self.geolocation_data:
-            self.geolocation_data.print_geolocation()
-
+            parts.append(f'message: {self.message}')
+        if self.protocol:
+            parts.append(f'protocol: {self.protocol}')
+        if self.scr_port:
+            parts.append(f'scr_port: {self.scr_port}')
+        if self.sensor:
+            parts.append(f'sensor: {self.sensor}')
+        if self.geolocation_data and not self.geolocation_data.is_null():
+            parts.append(str(self.geolocation_data))
+        if self.arch:
+            parts.append(f'arch: {self.arch}')
+        if self.duration:
+            parts.append(f'duration: {self.duration}')
+        if self.ssh_client_version:
+            parts.append(f'ssh_client_version: {self.ssh_client_version}')
+        if self.username:
+            parts.append(f'username: {self.username}')
+        if self.password:
+            parts.append(f'password: {self.password}')
+        if self.hasshAlgorithms:
+            parts.append(f'hasshAlgorithms: {self.hassh}')
+        if self.macCS:
+            parts.append(f'macCS: {self.macCS}')
+        if self.langCS:
+            parts.append(f'langCS: {self.langCS}')
+        if self.compCS:
+            parts.append(f'compCS: {self.compCS}')
+        if self.hassh:
+            parts.append(f'hassh: {self.hassh}')
+        if self.kexAlgs:
+            parts.append(f'kexAlgs: {self.kexAlgs}')
+        if self.keyAlgs:
+            parts.append(f'keyAlgs: {self.keyAlgs}')
+        if self.fingerprint:
+            parts.append(f'fingerprint: {self.fingerprint}')
+        if self.key:
+            parts.append(f'key: {self.key}')
+        if self.type:
+            parts.append(f'type: {self.type}')
+        if self.outfile:
+            parts.append(f'outfile: {self.outfile}')
+        if self.destfile:
+            parts.append(f'destfile: {self.destfile}')
+        if self.duplicate:
+            parts.append(f'duplicate: {self.duplicate}')
+        if self.shasum:
+            parts.append(f'shasum: {self.shasum}')
+        if self.url:
+            parts.append(f'url: {self.url}')
+        if self.ttylog:
+            parts.append(f'ttylog: {self.ttylog}')
+        if self.size:
+            parts.append(f'size: {self.size}')
+        if self.filename:
+            parts.append(f'filename: {self.filename}')
+        if self.data:
+            parts.append(f'data: {self.data}')
+        return "\n".join(parts)
 
 class ZenodoSession:
+    session_id: str
+    events: List[ZenodoEvent]
     def __init__(self, session_id: str, events: list[ZenodoEvent]):
+        if not session_id :
+            raise ValueError('session_id must not be None')
+        if not events :
+            raise ValueError('events must not be None')
         self.session_id = session_id
         self.events = events
 
-    def print_session(self):
-        print (f"Session ID: {self.session_id}")
+    def __str__(self):
+        parts = [f"Session ID: {self.session_id}"]
         for event in self.events:
-            event.print_event()
-
+            parts.append(str(event))
+        return "\n".join(parts)
 
 class ZenodoLog:
     def __init__(self, date_of_log):
@@ -142,9 +325,12 @@ class ZenodoLog:
                 events: list[ZenodoEvent] = []
 
                 for e in event_list:
-                    geo = e.get("geolocation_data")
-                    geo_data = None
+                    eventid = e.get("eventid")
+                    if not eventid:
+                        continue #eventid è required
 
+                    geo_data = None
+                    geo = e.get("geolocation_data")
                     if geo:
                         geo_data = GeolocationData(
                             postal_code=geo.get("postal_code"),
@@ -160,22 +346,41 @@ class ZenodoLog:
                             city_name=geo.get("city_name")
                         )
 
-                        event = ZenodoEvent(
-                            session_id=e.get("session_id", session_id),
-                            eventid=e.get("eventid"),
-                            timestamp=e.get("timestamp"),
-                            src_ip_identifier=e.get("src_ip_identifier"),
-                            dst_ip_identifier=e.get("dst_ip_identifier"),
-                            dst_host_identifier=e.get("dst_host_identifier"),
-                            message=e.get("message"),
-                            protocol=e.get("protocol"),
-                            src_port=e.get("src_port"),
-                            sensor=e.get("sensor"),
-                            geolocation_data=geo_data,
+                    event = ZenodoEvent(
+                        eventid=eventid,
+                        dst_host_identifier=e.get("dst_host_identifier"),
+                        timestamp=e.get("timestamp"),
+                        src_ip_identifier=e.get("src_ip_identifier"),
+                        dst_ip_identifier=e.get("dst_ip_identifier"),
+                        message=e.get("message"),
+                        protocol=e.get("protocol"),
+                        scr_port=e.get("src_port"),
+                        sensor=e.get("sensor"),
+                        geolocation_data=geo_data,
+                        arch=e.get("arch"),
+                        duration=e.get("duration"),
+                        ssh_client_version=e.get("ssh_client_version"),
+                        username=e.get("username"),
+                        password=e.get("password"),
+                        macCS=e.get("macCS"),
+                        encCS=e.get("encCS"),
+                        kexAlgs=e.get("kexAlgs"),
+                        keyAlgs=e.get("keyAlgs")
                         )
-                        events.append(event)
-                    log.sessions.append(ZenodoSession(session_id=session_id, events=events))
+
+                    events.append(event)
+
+                    if events:
+                        log.sessions.append(ZenodoSession(session_id, events))
+
         return log
+
+
+    def __str__(self):
+        parts = ["Zenodo Log:", f"data log: {self.date_of_log}"]
+        for session in self.sessions:
+            parts.append(str(session))
+        return "\n".join(parts)
 
     @staticmethod
     def parse_date_from_filename(filename: str) -> str: # es: cyberlab_2019-05-13.json -> 2019-05-13
@@ -185,4 +390,4 @@ class ZenodoLog:
     def print_log(self):
         print(f"Date of Log: {self.date_of_log}")
         for s in self.sessions:
-            s.print_session()
+            print(s)
--- HoneyClusterCode/Zenodo/ZenodoDownloader.py
+++ HoneyClusterCode/Zenodo/ZenodoDownloader.py
@@ -1,10 +1,12 @@
 import json
 import logging
+
+
 from Utils import file_worker
 import os
 from ZenodoDataStructure import ZenodoLog
 
-class ZenodoInterpreter:
+class ZenodoDownloader:
     def __init__(self, zenodo_local_path: str , extracted_zenodo_local_path : str | None):
         logging.info("Zenodo Interpreter started.")
         if file_worker.contains_file_type(zenodo_local_path, ".gz") > 0 :
@@ -12,6 +14,36 @@ class ZenodoInterpreter:
 
         else:
             logging.info("Zenodo Interpreter failed because of error in path.")
+
+
+    """
+            def download_zenodo_dataset(self, record_id):
+        api_url = f"https://zenodo.org/api/records/{record_id}"
+
+        try:
+            response = requests.get(api_url)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            logging.error(f"Errore nel contattare Zenodo {api_url}: {e}")
+            return []
+        try:
+            meta_record = response.json()
+        except ValueError as e:
+            logging.error(f"Errore nella risposta di Zenodo. Non è un json valido: {e}")
+            return []
+
+        files = meta_record.get("files", [])
+        if not isinstance(files, list):
+            logging.error("Campo 'files' mancante o non valido nella risposta Zenodo")
+            return []
+
+        gz_files = [f for f in files if f.get("key", "").endswith(".gz")]
+        return gz_files
+
+    """
+
+
+
 
     def interpret_single_zenodo_json(self, filename: str):
         try:
@@ -21,15 +53,16 @@ class ZenodoInterpreter:
             date = ZenodoLog.parse_date_from_filename(filename)
             log = ZenodoLog.from_json(date, json_data)
 
-            log.print_log()
+            print(log)
 
         except Exception as e:
             logging.error(f"Failed to parse JSON {filename}: {e}")
 
 def main() -> None:
     logging.basicConfig(level=logging.DEBUG)
-    z_interpreter = ZenodoInterpreter("C:\\Users\\Sveva\\Documents\\GitHub\\zenodo_dataset", "C:\\Users\\Sveva\\Desktop\\estratti")
+    z_interpreter = ZenodoDownloader("C:\\Users\\Sveva\\Documents\\GitHub\\zenodo_dataset", "C:\\Users\\Sveva\\Desktop\\estratti")
     z_interpreter.interpret_single_zenodo_json(os.path.join(z_interpreter.extracted_zenodo_path, "cyberlab_2019-05-18.json"))
+
 
 if __name__ == "__main__":
     main()
